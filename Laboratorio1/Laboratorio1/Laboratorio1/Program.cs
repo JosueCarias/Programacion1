72d17609e84c0892312f87b9d88b9ef03f2adbd5
JosueCarias <84352141+JosueCarias@users.noreply.github.com>
2025-02-09 23:16:36 -0600

    Problemas 3,4 y documentacion

--- Laboratorio1/Laboratorio1/Laboratorio1/Program.cs
+++ Laboratorio1/Laboratorio1/Laboratorio1/Program.cs
@@ -4,11 +4,13 @@ namespace MiProyecto
 {
     class Program
     {
+        //Contructor principal
         static void Main(string[] args)
         {
             MostrarMenu(); 
         }
 
+        //Funcion que ejecuta el menu principal
         static void MostrarMenu()
         {
             Console.Clear();
@@ -23,10 +25,10 @@ namespace MiProyecto
                     sueldo();
                     break;
                 case 3:
-                    
+                    numeroIntermedio();
                     break;
                 case 4:
-                    
+                    Combinacion();
                     break;
                 case 5:
                     ;
@@ -74,6 +76,7 @@ namespace MiProyecto
             MostrarMenu();
         }
 
+        //Funcion que calcular el año bisiesto #1
         static void anoBisiesto()
         {
             int año = 0;
@@ -94,6 +97,7 @@ namespace MiProyecto
             Console.WriteLine("-----------------------------------------------");
         }
 
+        //Funcion que calcula es sueldo #2
         static void sueldo()
         {
             double sueldoBase = 0;
@@ -111,6 +115,7 @@ namespace MiProyecto
 
         }
 
+        //Funcion complemento de sueldo
         static double cuotaLaboral(double sueldoBase)
         {
             double cuotaLaboral = 0;
@@ -119,6 +124,7 @@ namespace MiProyecto
             return cuotaLaboral;
         }
 
+        //Funcion complemento de sueldo
         static double cuotaPatronal(double sueldoBase)
         {
             double cuotaPatronal = 0;
@@ -127,6 +133,7 @@ namespace MiProyecto
             return cuotaPatronal;
         }
 
+        //Funcion complemento de sueldo
         static double sueldoLiquido(double sueldoBase)
         {
             double sueldoLiquido = 0;
@@ -135,12 +142,84 @@ namespace MiProyecto
             return sueldoLiquido;
         }
 
+        //Funcion que calcular el numero intermedio #3
+        static void numeroIntermedio()
+        {
+            Console.WriteLine("-----------------------------------------------");
+
+            int num1, num2, num3;
+
+            Console.WriteLine("Ingrese El Primer Numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese El Segundo Numero");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese El Tercer Numero");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if ((num1 > num2 && num1 < num3) || (num1 < num2 && num1 > num3))
+            {
+                Console.WriteLine($"El número {num1} es el número intermedio.");
+            }
+            else if ((num2 > num1 && num2 < num3) || (num2 < num1 && num2 > num3))
+            {
+                Console.WriteLine($"El número {num2} es el número intermedio.");
+            }
+            else if ((num3 > num1 && num3 < num2) || (num3 < num1 && num3 > num2))
+            {
+                Console.WriteLine($"El número {num3} es el número intermedio.");
+            }
+            else
+            {
+                Console.WriteLine("los números son iguales");
+            }
+        }
+
+        //Funcion combinacion #4
+        static void Combinacion()
+        {
+            Console.WriteLine("------------Funcion Combinacion------------");
+
+            int n, r, resultado;
+
+            Console.WriteLine("Ingrese el valor de N:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de R:");
+            r = Convert.ToInt32(Console.ReadLine());
+
+            if (n >= r)
+            {
+                resultado = factorial(n) / (factorial(r) * factorial(n - r));
+                Console.WriteLine("Resultado de la combinación: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("Error: N debe ser mayor o igual que R.");
+            }
+
+            Console.WriteLine("--------------------------------------");
+        }
+
+        //Funcion completo de combinacion
+        public static int factorial(int n)
+        {
+            int factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        //Funcion para salir #16
         static void salir()
         {
             Console.WriteLine("Saliendo del programa...");
-            Environment.Exit(0); // Terminar el programa
+            Environment.Exit(0); 
         }
 
+        //Fucion que genera el menu
         public static int menu()
         {
             int opcion = 0;
@@ -148,7 +227,7 @@ namespace MiProyecto
             Console.WriteLine("1) Año Bisiesto:");
             Console.WriteLine("2) Cuota Laboral:");
             Console.WriteLine("3) Numero intermedio:");
-            Console.WriteLine("4) Formula n!/(r!(n-r)!):");
+            Console.WriteLine("4) Combinacion:");
             Console.WriteLine("5) Finero Expresada En Quetzales:");
             Console.WriteLine("6) INE:");
             Console.WriteLine("7) Calcule El Sueldo Diario:");
