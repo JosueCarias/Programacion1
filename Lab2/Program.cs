0d0ec1511cf8bf58481838fed3f939d51a70f778
JosueCarias <84352141+JosueCarias@users.noreply.github.com>
2025-02-24 21:02:58 -0600

    Lab 2 problema 1

---
+++ Lab2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Lab2;
+
+namespace MiProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu();
+        }
+        static void menu(){
+            Console.WriteLine("----------Menu principal----------");
+            Console.WriteLine("1. Viaje de estudios");
+            Console.WriteLine("2. Dinero en quetzales");
+            Console.WriteLine("3. Numero perfecto");
+            Console.WriteLine("4. Edad 3 Personas");
+            Console.WriteLine("5. Numero entero positivo o negativo");
+            Console.WriteLine("6. Volumen de un cilindro");
+            Console.WriteLine("7. Marcador de equipo");
+            Console.WriteLine("8. Maximo comun divisor");
+            Console.WriteLine("9. Serie de Fibonacci");
+            Console.WriteLine("10. Tablas de multiplicar");
+            Console.WriteLine("11. Salir");
+            Console.WriteLine("Elija una opcion");
+            int opcion =Convert.ToInt32(Console.ReadLine());
+            
+            switch(opcion){
+                case 1: viaje();menu();break;
+                case 2: menu();break;
+                case 3: menu();break;
+                case 4: menu();break;
+                case 5: menu();break;
+                case 6: menu();break;
+                case 7: menu();break;
+                case 8: menu();break;
+                case 9: menu();break;
+                case 10: menu();break;
+                case 11: Console.ReadKey();break;
+                default: Console.WriteLine("Opcion Incorrecta");menu();break;
+            }
+        }
+        public static void viaje(){
+            Console.WriteLine("----------Calculo de costo de viaje en bus----------");
+            Console.WriteLine("Ingrese la cantidad de alumnos:");
+            int nAlumnos = Convert.ToInt32(Console.ReadLine());
+            csPagoAutobus pago =new csPagoAutobus(nAlumnos);
+            Console.WriteLine($"El pago total es: {pago.calcularPagoAutoBus()}");
+            Console.WriteLine($"El pago individual es: {pago.calcularPagoIndividual()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2/csPagoAutobus.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Formats.Tar;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Lab2
+{
+    public class csPagoAutobus
+    {
+        private int nAlumnos;
+
+        public csPagoAutobus(int nAlumnos){
+            this.nAlumnos=nAlumnos;
+        }
+       public int NAlumnos
+        {
+            get { return nAlumnos; }
+            set { nAlumnos = value; }
+        }
+       public int calcularPagoAutoBus(){
+        int pago=0;
+        if (nAlumnos>=100){
+            pago=nAlumnos*65;
+        }else if(nAlumnos<=99 && nAlumnos>=50){
+            pago=nAlumnos*70;
+        }else if(nAlumnos<=49 && nAlumnos>=30){
+            pago=nAlumnos*95;
+        }else if(nAlumnos<=30){
+            pago=4000;
+        }
+        return  pago;
+       }
+       public int calcularPagoIndividual(){
+        int pago=0;
+        pago=calcularPagoAutoBus()/nAlumnos;
+        return pago;
+       }
+    }
+}
